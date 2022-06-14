30b1661e6cbaa52ab1cf2ad334ba655191c82059
Simran Preet Singh <simran5590@gmail.com>
2022-06-14 07:51:25 -0700

    Initial Commit

---
+++ InstaFilters/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstaFilters
+//
+//  Created by Simran Preet Narang on 2022-06-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstaFilters/InstaFiltersApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFiltersApp.swift
+//  InstaFilters
+//
+//  Created by Simran Preet Narang on 2022-06-14.
+//
+
+import SwiftUI
+
+@main
+struct InstaFiltersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
