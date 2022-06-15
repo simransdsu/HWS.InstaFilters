cb719e779d04db3f99f5070b853ce7e5024b2771
Simran Preet Singh <simran5590@gmail.com>
2022-06-15 07:15:27 -0700

    Using Core Image

--- InstaFilters/ContentView.swift
+++ InstaFilters/ContentView.swift
@@ -6,11 +6,37 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    
+    @State private var image: Image?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear {
+            loadImage()
+        }
+    }
+    
+    func loadImage() {
+        guard let inputImage = UIImage(named: "1") else { return }
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.sepiaTone()
+        currentFilter.inputImage = beginImage
+        currentFilter.intensity = 1.0
+        
+        guard let outputCIImage =  currentFilter.outputImage else { return }
+        guard let outputCGImage = context.createCGImage(outputCIImage, from: outputCIImage.extent) else { return }
+        let outputImage = UIImage(cgImage: outputCGImage)
+        image = Image(uiImage: outputImage)
     }
 }
 
